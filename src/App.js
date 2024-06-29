438fb10441e0b70982c77d1d40ec0fadc0e58ea9
jaquesaurelio2 <jaquesaurelio1@gmail.com>
2024-06-29 16:22:54 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import "./styles.css";
+
+export default function App() {
+  const numbers = [0, 1, 2, 3, 4, 5];
+  const words = ["brito", "rayann", "fast"];
+
+  return <div className="App"></div>;
+}
